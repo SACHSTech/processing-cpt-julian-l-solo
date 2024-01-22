d938baae2fd6182b7d8064615947d971df7abbb4
Julian3377 <julianli3377@gmail.com>
2024-01-21 19:46:06 -0500

    Made swift and C bosses

--- Sketch.java
+++ Sketch.java
@@ -4,7 +4,7 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
 
   int state = 0;
-  int lives = 0;	
+  int lives = 4;	
   PImage backgroundImage;
 	
   /**
@@ -26,16 +26,14 @@ public class Sketch extends PApplet {
     background(210, 255, 173);
     if (state == 0) {
       image(backgroundImage, 0, 0, width, height);
-
       textAlign(CENTER, CENTER);
       textSize(80);
       rect(450, 90, 500, 100);
       fill(255);
       text("CATQUEST", width / 2, 80);
-
       drawButton("Play", width / 2, 200, 200, 50);
       drawButton("Controls", width / 2, 300, 200, 50);
-      drawButton("Attacks", width / 2, 400, 200, 50);
+      drawButton("Settings", width / 2, 400, 200, 50);
     }
 
     if (state == 1) {
@@ -47,31 +45,13 @@ public class Sketch extends PApplet {
       text("CONTROLS", 450, 80);
       textSize(20);
       text("WASD to move", 450, 180);
-      text("DRAG MOUSE to aim", 450, 230);
-      text("RIGHT CLICK to cycle attacks", 450, 280);
-      text("E for special attack [Costs 1 mouse]", 450, 330);
-      text("R to activate nine lives [Costs 5 mice]", 450, 380);
-      text("Goal: Defeat the boss, avoid losing all lives.", 450, 430);
+      text("ARROW KEYS to move drone", 450, 230);
+      text("Goal: Defeat the boss, avoid losing all lives.", 450, 280);
+      text("Your drone is your attacker, move them to attack the boss.", 450, 330);
+      text("Careful, your drone resets every once in a while.", 450, 380);
     }
 
     if (state == 2) {
-      background(0);
-      drawButton("Back", 70, 50, 100, 50);
-      textAlign(CENTER, CENTER);
-      textSize(50);
-      fill(255);
-      text("ATTACKS", 450, 80);
-      textSize(20);
-      text("CLAW LAUNCHER - A default, fast shooting weapon suitable for general use.", 450, 150);
-      text("\tSPECIAL - Gives a rotating claw shield that removes projectiles.", 450, 180);
-      text("KIBBLE BURST - A quick but small shotgun that can do wonders up close.", 450, 250);
-      text("\tSPECIAL - Releases powerful kibble in all directions.", 450, 280);
-      text("CATNIP CANNON - A devastating, piercing shot. Slow, but deadly.", 450, 350);
-      text("\tSPECIAL - Sends an even more devastating blast, though it doesn’t pierce.", 450, 380);
-      text("NINE LIVES - Can heal 1 hp by spending 5 mice.", 450, 450);
-    }
-
-    if (state == 3) {
       background(0);
       drawButton("Back", 70, 50, 100, 50);
       textAlign(CENTER, CENTER);
@@ -85,7 +65,7 @@ public class Sketch extends PApplet {
       drawButton("Hard", 450, 430, 200, 50);
     }
 
-    if (state == 4) {
+    if (state == 3) {
       background(0);
       drawButton("Back", 70, 50, 100, 50);
       textAlign(CENTER, CENTER);
@@ -126,35 +106,35 @@ public class Sketch extends PApplet {
     else if (label == "Controls") {
       state = 1;
     }
-    else if (label == "Attacks") {
+    else if (label == "Settings") {
       state = 2;
     }
     else if (label == "Play") {
       state = 3;
     }
     else if (label == "Easy") {
-      lives = 10;
-      state = 4;
+      lives = 4;
+      state = 0;
     }
     else if (label == "Normal") {
-      lives = 6;
-      state = 4;
+      lives = 3;
+      state = 0;
     }
     else if (label == "Hard") {
-      lives = 3;
-      state = 4;
+      lives = 1;
+      state = 0;
     }
     else if (label == "Tutorial Tabby") {
-      state = 5;
+      state = 4;
     }
     else if (label == "Script Kitty") {
-      state = 6;
+      state = 5;
     }
     else if (label == "Clockpurrk") {
-      state = 7;
+      state = 6;
     }
     else if (label == "The Mainframe") {
-      state = 8;
+      state = 7;
     }
   }
 
--- Sketch1.java
+++ Sketch1.java
@@ -2,30 +2,307 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch1 extends PApplet {
-	
-  PImage background1;
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  PImage catimg;
+  PImage drone;
+  PImage swift;
+  PImage swiftAlt;
+  PImage miniSwift;
+  PImage swiftOrb;
+
+  float playerX = 100f;
+  float playerY = 410f;
+  int lives = 4;
+  double gravity = 0.5;
+  float verticalVelocity = 0;
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+  boolean isJumping = false;
+
+  float droneX = 100;
+  float droneY = 410;
+  float droneDirectionX = 5;
+  float droneDirectionY = 0;
+  float droneCooldown = 0;
+
+  int bossX = 600;
+  int bossY = 200;
+  boolean facingLeft = true;
+  int bossMovement = 5;
+  float bossHealth = 100;
+  boolean recentDamage = false;
+
+  float proj1X = 0;
+  float proj1Y = 0;
+  float proj2X = 0;
+  float proj2Y = 0;
+  boolean warning = false;
+
+  int phase = 0;
+
   public void settings() {
-	// put your size call here
     size(900, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    catimg = loadImage("catcharacter.png");
+    drone = loadImage("catDrone.png");
+    swift = loadImage("swiftRight.png");
+    swiftAlt = loadImage("swiftLeft.png");
+    miniSwift = loadImage("birdMinion.png");
+    swiftOrb = loadImage("swiftOrb.png");
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	   
-  }
+    background(255);
+    image(catimg, playerX, playerY, 60, 90);
+    if (facingLeft == true) {
+      bossX = 600;
+      bossY = 200;
+      image(swift, bossX, bossY, 300, 300);
+    } else {
+      bossX = 0;
+      bossY = 200;
+      image(swiftAlt, bossX, bossY, 300, 300);
+    }
+
+    fill(255, 255, 0);
+    rect(15, 10, 80, 30);
+    fill(0);
+    textAlign(LEFT, TOP);
+    textSize(18);
+    text("LIVES: " + lives, 20, 15);
+
+    if (phase == 0) {
+      fill(0);
+      textAlign(CENTER, CENTER);
+      textSize(100);
+      text("READY?", 450, 250);
+      if (frameCount % 120 == 0) {
+        phase += 1;
+      }
+    }
+
+    if (phase == 1) {
+      fill(0);
+      textAlign(CENTER, CENTER);
+      textSize(100);
+      text("GO!", 450, 250);
+
+      if (frameCount % 180 == 0) {
+        phase += 1;
+      }
+    }
+    
+    if (phase == 2) {
+      
+      if (frameCount % 240 == 0) {
+        if (facingLeft) {
+          proj1X = bossX;
+          proj1Y = bossY + 200;
+          proj2X = bossX + 50;
+          proj2Y = bossY + 250;
+        } else {
+          proj1X = bossX + 50;
+          proj1Y = bossY + 200;
+          proj2X = bossX + 100;
+          proj2Y = bossY + 250;
+        }
+      }
+
+      if (proj1X != 0 && proj1Y != 0) {
+        image(miniSwift, proj1X, proj1Y, 50, 50);
+        if (facingLeft == true) {
+          proj1X -= 10;
+        } else {
+          proj1X += 10;
+        }
+      }
+
+      if (proj2X != 0 && proj2Y != 0) {
+        if (facingLeft == true) {
+          proj2X -= 3;
+        } else {
+          proj2X += 3;
+        }
+        image(swiftOrb, proj2X, proj2Y, 40, 40);
+      }
+
+      if (proj1X > width || proj1X < 0 || proj1Y > height || proj1Y < 0) {
+        proj1X = 0;
+        proj1Y = 0;
+      }
+
+      if (frameCount % 630 == 0) {
+        warning = true;
+      }
+
+      if (warning == true) {
+        fill(0);
+        textSize(20);
+        text("WARNING", 800, 0);
+      }
+
+      if (frameCount % 750 == 0) {
+        if (facingLeft == true) {
+          facingLeft = false;
+        } else {
+          facingLeft = true;
+        }
+        warning = false;
+      }
+      
+      if (lives == 0) {
+        background(255);
+        noLoop();
+
+        fill(255, 0, 0);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Game Over :(", 450, 250);
+
+        return;
+      }
+
+      if (bossHealth <= 0) {
+        background(255);
+        noLoop();
+
+        fill(255, 0, 0);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("KNOCKOUT!", 450, 250);
+
+        return;
+      }
+
+      if (checkCollision(playerX, playerY, bossX, bossY, 200) && recentDamage == false) {
+        playerDamage();
+      }
+
+      if (checkCollision(playerX, playerY, proj2X, proj2Y, 200) && recentDamage == false) {
+        playerDamage();
+      }
+
+      if (checkCollision(playerX, playerY, proj1X, proj1Y, 45) && recentDamage == false) {
+        playerDamage();
+      }
+
+      if (checkCollision(droneX, droneY, bossX, bossY, 200)) {
+        bossHealth -= 0.1;
+      }
   
+      // Makes it so there's a delay before user can get damaged again
+      if (recentDamage) {
+        if (frameCount % 120 == 0) {
+          recentDamage = false;
+        }
+      }
+  
+      if (keyPressed) {
+        if (upPressed && isJumping == false) {
+          verticalVelocity = -12;
+          isJumping = true;
+        }
+        if (leftPressed) {
+          if (playerX >= 0) {
+            playerX -= 4;
+          }
+        }
+        if (rightPressed) {
+          if (playerX <= 840) {
+            playerX += 4;
+          }
+        }
+      }
+      
+      playerY += verticalVelocity;
+      verticalVelocity += gravity;
+  
+      if (playerY >= 410) {
+        playerY = 410;
+        isJumping = false;
+        verticalVelocity = 0;
+      }
+  
+      droneX += droneDirectionX;
+      droneY += droneDirectionY;
+  
+      image(drone, droneX, droneY, 40, 60);
+  
+      if (droneX > width || droneX < 0 || droneY > height || droneY < 0) {
+        resetClaw();
+        droneCooldown = frameCount;
+      }
+  
+      if (frameCount - droneCooldown > 600) {
+        resetClaw();
+        droneCooldown = frameCount;
+      }
+    }
+  }
+
+  public void keyPressed() {
+    if (key == 'w') {
+      upPressed = true;
+    }
+    else if (key == 's') {
+      downPressed = true;
+    }
+    else if (key == 'a') {
+      leftPressed = true;
+    }
+    else if (key == 'd') {
+      rightPressed = true;
+    }
+    
+    if (keyCode == UP) {
+      droneDirectionX = 0;
+      droneDirectionY = -4;
+    } else if (keyCode == DOWN) {
+      droneDirectionX = 0;
+      droneDirectionY = 4;
+    } else if (keyCode == LEFT) {
+      droneDirectionX = -4;
+      droneDirectionY = 0;
+    } else if (keyCode == RIGHT) {
+      droneDirectionX = 4;
+      droneDirectionY = 0;
+    }
+  }
+
+  public void keyReleased() {
+    if (key == 'w') {
+      upPressed = false;
+    }
+    else if (key == 's') {
+      downPressed = false;
+    }
+    else if (key == 'a') {
+      leftPressed = false;
+    }
+    else if (key == 'd') {
+      rightPressed = false;
+    }
+  }
+
+  void resetClaw() {
+    droneX = playerX;
+    droneY = playerY + 30;
+  }
+
+  boolean checkCollision(float x1, float y1, float x2, float y2, float distance) {
+    if (x1 > 0 && x2 < width && y2 > 0 && y2 < height) {
+      return dist(x1, y1, x2 + (distance / 2), y2 + (distance / 2)) < distance;
+    }
+    return false;
+  }
+
+  void playerDamage() {
+    background(255, 0, 0);
+    lives -= 1;
+    recentDamage = true;
+  }
 }
